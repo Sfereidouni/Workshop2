ad114cdb37458c6453561fe62ac8f7420381d326
David Reeves <darthur.reeves@gmail.com>
2017-11-25 14:27:18 +0000

    Add SharedSandbox

---
+++ Unity/Projects/SharedSandbox/Assets/MeshIntro/Scripts/CreateMesh2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using MyAwesomeLibrary;
+
+
+/// <summary>
+/// 
+/// </summary>
+public class CreateMesh2 : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+        MeshFilter filter = GetComponent<MeshFilter>();
+        filter.sharedMesh = MeshFactory.CreateTexturedQuad();
+	}
+}
